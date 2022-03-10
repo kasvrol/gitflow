e933559cb1566d0c86b5735a0caa8d9e869283d6
kasvrol <dovalekarolina@gmail.com>
2022-03-10 12:10:12 -0300

    [FEATURE 008] [ARQUIVOS] Adição dos arquivos react para inicio do projeto*

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header>
+        <p>Hello, it's me</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
